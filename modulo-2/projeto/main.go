fb19a33d097cb5dcdb7d7977cf6d8044278416a4
chauchuty <cesar.chauchuty@gmail.com>
2025-04-19 11:05:57 -0300

    update: projeto final

---
+++ modulo-2/projeto/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type Measurement struct {
+	Min   float64
+	Max   float64
+	Sum   float64
+	Count int64
+}
+
+func main() {
+	measurements, err := os.Open("weather_stations.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer measurements.Close()
+
+	dados := make(map[string]Measurement)
+
+	scanner := bufio.NewScanner(measurements)
+	for scanner.Scan() {
+		rawData := scanner.Text()
+		semicolon := strings.Index(rawData, ";")
+		location := rawData[:semicolon]
+		rawTemp := rawData[semicolon+1:]
+		temp, _ := strconv.ParseFloat(rawTemp, 64)
+
+		measurement, ok := dados[location]
+		if !ok {
+			measurement = Measurement{
+				Min:   temp,
+				Max:   temp,
+				Sum:   temp,
+				Count: 1,
+			}
+		} else {
+			measurement = Measurement{
+				Min:   measurement.Min,
+				Max:   measurement.Max,
+				Sum:   measurement.Sum,
+				Count: measurement.Count,
+			}
+		}
+
+		measurement.Min = min(measurement.Min, temp)
+		measurement.Max = max(measurement.Max, temp)
+		measurement.Sum += temp
+		measurement.Count++
+
+		dados[location] = measurement
+	}
+
+	locations := make([]string, 0, len(dados))
+	for name := range dados {
+		locations = append(locations, name)
+	}
+
+	sort.Strings(locations)
+
+	for _, name := range locations {
+		measurement := dados[name]
+		fmt.Printf("%s: %.1f/%.1f/%.1f\n",
+			name,
+			measurement.Min,
+			measurement.Max,
+			measurement.Sum/float64(measurement.Count),
+		)
+	}
+}
