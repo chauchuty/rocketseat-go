80f9d7ac31b4c7885db54f670b8748e6a002cadf
chauchuty <cesar.chauchuty@gmail.com>
2025-04-19 07:46:18 -0300

    update: goroutine part2

--- modulo-2/goroutineContext/main.go
+++ modulo-2/goroutineContext/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"sync"
 	"time"
 )
@@ -13,17 +16,37 @@ func main() {
 	var wg sync.WaitGroup
 	wg.Add(n)
 
-	for range n {
-		go func() {
-			defer wg.Done()
-			res, err := http.Get("https://google.com")
-			if err != nil {
-				panic(err)
-			}
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Millisecond)
+	defer cancel()
 
+	server := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(10 * time.Second)
+			fmt.Fprintln(w, "Hello, World!")
+		}),
+	)
+
+	for range n {
+		go func(ctx context.Context) {
+			defer wg.Done()
+
+			req, err := http.NewRequestWithContext(
+				ctx,
+				"GET",
+				server.URL,
+				nil,
+			)
+
+			res, err := http.DefaultClient.Do(req)
+			if err != nil {
+				if errors.Is(err, context.DeadlineExceeded) {
+					fmt.Println("timeout")
+					return
+				}
+			}
 			defer res.Body.Close()
-			fmt.Println("ok")
-		}()
+		}(ctx)
 	}
 	wg.Wait()
 	fmt.Println(time.Since((start)))
